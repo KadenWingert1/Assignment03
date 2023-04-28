3feac865606bef3f76150d62d3c2828a12cb0498
Bryce <bmaloy19@gmail.com>
2023-04-28 10:31:27 -0500

    Better working add.js, simple css
    setting categories right along with photos. going to getting it so it pulls correct photos on view all page.

--- team48_3frontend/src/add.js
+++ team48_3frontend/src/add.js
@@ -10,10 +10,40 @@ function Add({ showAddView }) {
     rating: { rate: 0, count: 0 },
   });
 
+  // Category-image mapping
+  const categoryImages = {
+    "Pocket Knives": [
+      "pocketKnife.png",
+      "fanghornPocketKnife.png",
+      "wispPocketKnife.png",
+    ],
+    Daggers: ["tolkienDagger.png", "kunaiDagger.png"],
+    Swords: ["heleldrSword.png", "seaxSword.png", "katanaSword.png"],
+    Resin: [
+      "mapleLeafResin.png",
+      "redwoodResin.png",
+      "oceanResin.png",
+      "buckeyeResin.png",
+    ],
+    Jewelry: ["mjolnirJewelry.png"],
+    Custom: ["custom.png"],
+  };
+
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  // Function to handle category change
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+    setProduct({
+      ...product,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   const [productId, setProductId] = useState(1); // or whatever starting number you like
 
   function addProduct(e) {
-    e.preventDefault();
+    //e.preventDefault();
 
     const newProduct = {
       ...product,
@@ -54,6 +84,7 @@ function Add({ showAddView }) {
         value={product.title}
         onChange={handleChange}
         placeholder="Product Title"
+        className="input"
       />
       <input
         name="price"
@@ -61,31 +92,35 @@ function Add({ showAddView }) {
         value={product.price}
         onChange={handleChange}
         placeholder="Product Price"
+        className="input"
       />
       <input
         name="description"
         value={product.description}
         onChange={handleChange}
         placeholder="Product Description"
+        className="input"
       />
       <label>Category</label>
-      <select name="category" id="category">
-  <option value="">--Select Category--</option>
-  <option value="Pocket Knives">Pocket Knives</option>
-  <option value="Daggers">Daggers</option>
-  <option value="Swords">Swords</option>
-  <option value="Resin">Resin</option>
-  <option value="Jewelry">Jewelry</option>
-  <option value="Custom">Custom</option>
-</select>
-
+      <select name="category" id="category" onChange={handleCategoryChange} className="select">
+        <option value="">--Select Category--</option>
+        <option value="Pocket Knives">Pocket Knives</option>
+        <option value="Daggers">Daggers</option>
+        <option value="Swords">Swords</option>
+        <option value="Resin">Resin</option>
+        <option value="Jewelry">Jewelry</option>
+        <option value="Custom">Custom</option>
+      </select>
 
       <label>Image</label>
-      <select name="image" required>
+      <select name="image" required onChange={handleChange} className="select">
         <option value="">Select an image</option>
-        <option value="swords.jfif">Swords.jfif</option>
-        <option value="seaxSword.png">SeaxSword.png</option>
-        <option value="katanaSword.png">KatanaSword.png</option>
+        {selectedCategory &&
+          categoryImages[selectedCategory].map((image) => (
+            <option key={image} value={image}>
+              {image}
+            </option>
+          ))}
       </select>
 
       <input
@@ -94,6 +129,7 @@ function Add({ showAddView }) {
         value={product.rating.rate}
         onChange={handleNestedChange}
         placeholder="Rating Rate"
+        className="input"
       />
       <input
         name="count"
@@ -101,6 +137,7 @@ function Add({ showAddView }) {
         value={product.rating.count}
         onChange={handleNestedChange}
         placeholder="Rating Count"
+        className="input"
       />
       <button type="submit">Add Product</button>
     </form>
